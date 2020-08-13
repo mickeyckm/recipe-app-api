fe27eaab58574c313fa9dbe9b6431e7a6dfaf019
Mickey Cheong <mickey.cheong@sendbird.com>
2020-08-13 11:58:28 +0800

    Added custom recipe detail view

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -22,7 +22,7 @@ class IngredientSerializer(serializers.ModelSerializer):
 
 
 class RecipeSerializer(serializers.ModelSerializer):
-    """Serizliaer for recipe objects"""
+    """Serializer for recipe objects"""
 
     ingredients = serializers.PrimaryKeyRelatedField(
         many=True,
@@ -41,3 +41,11 @@ class RecipeSerializer(serializers.ModelSerializer):
             'price', 'link'
         )
         read_only_fields = ('id',)
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe object"""
+
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
+
